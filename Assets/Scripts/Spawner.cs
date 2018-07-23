71d7e3f74a84ec886753ec8c88366b63b81e88cf
Zulou <rmil.cb@gmail.com>
2018-07-23 15:53:39 -0500

    initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	
+	public GameObject[] groups;
+	
+	
+	void Start () {
+		spawnNext ( ) ; 
+	}
+	void Update () {
+		
+	}
+	 public void spawnNext ( ) {
+		// Random Index
+		int i = Random.Range( 0 , groups.Length) ;
+
+		// Spawn Group at current Position
+		Instantiate ( groups[ i ],transform.position ,Quaternion.identity ) ;
+	} 
+}
